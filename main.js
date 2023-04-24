0ea86cde305dcadb830527c6611dbc108b59efe6
Rehab <rehababdelaleem.97@gmail.com>
2023-04-25 00:41:04 +0200

    landing page for apple store

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector(".img");
+let conteiner = document.querySelector(".container");
+function phones(src) {
+  img.src = src;
+}
+function colors(color) {
+  conteiner.style.background = color;
+}
